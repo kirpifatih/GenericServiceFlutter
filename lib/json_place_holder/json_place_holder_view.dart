516ff7e180194df54645e49dc3bcf8895091d3b7
fatihkirpi <extfatihk@d-teknoloji.com.tr>
2021-06-25 14:56:13 +0300

    listview added

--- lib/json_place_holder/json_place_holder_view.dart
+++ lib/json_place_holder/json_place_holder_view.dart
@@ -1,11 +1,16 @@
-
 import 'package:flutter/material.dart';
 import 'json_place_holder_view_model.dart';
 
 class JsonPlaceHolderView extends JsonPlaceHolderViewModel{
   @override
   Widget build(BuildContext context) {
-    return Text(" just Placeholder");
+    return Scaffold(
+      appBar: AppBar(),
+      body: ListView.builder(
+        itemCount: postModel.length,
+        itemBuilder: (context,index)=>Text(postModel[index].title)),
+    );
+    
   }
 
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:http_example/json_place_holder/json_place_holder.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({ Key key }) : super(key: key);
+  const MyApp({Key key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) { 
+  Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title:'Material App',
-      home: Scaffold(appBar: AppBar(title: Text('Generic Api'),
-      ),
-      body: Center(child: Container(child: Text("Hello Word"),),),
-      ),
-      
+      title: 'Material App',
+      home: JsonPlaceHolder(),
     );
   }
 }
